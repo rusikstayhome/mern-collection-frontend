53646f2af1f541234b2156086115fcb9c33fd4da
rusikstayhome <alenrus32@gmail.com>
2022-10-14 12:09:58 +0300

    added axios

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,7 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Container, Row, Col, Button } from 'react-bootstrap'
+
+import axios from '../../axios'
 
 import Item from '../../components/Item/Item'
 import Tags from '../../components/Tags/Tags'
@@ -8,53 +10,58 @@ import Collection from '../../components/Collection/Collection'
 import './Home.css'
 
 const Home = () => {
+  useEffect(() => {
+    axios.get('/collections')
+  }, []);
 
-    const [showItems, setShowItems] = useState(true);
+
+
+  const [showItems, setShowItems] = useState(true);
 
 
 
-    return (
-        <Container>
-            <div className="home mb-4">
-                <span onClick={() => setShowItems(true)} className={showItems && 'active'}>Last Items</span>
-                <span onClick={() => setShowItems(false)} className={!showItems && 'active'}>Top Collections</span>
-            </div>
+  return (
+    <Container>
+      <div className="home mb-4">
+        <span onClick={() => setShowItems(true)} className={showItems && 'active'}>Last Items</span>
+        <span onClick={() => setShowItems(false)} className={!showItems && 'active'}>Top Collections</span>
+      </div>
+      <Row>
+        <Col sm={8}>
+          {showItems ? (
             <Row>
-                <Col sm={8}>
-                    {showItems ? (
-                        <Row>
-                            <Col className='col-6'>
-                                <Item />
-                            </Col>
-                            <Col className='col-6'>
-                                <Item />
-                            </Col>
-                            <Col className='col-6'>
-                                <Item />
-                            </Col>
-                            <Col className='col-6'>
-                                <Item />
-                            </Col>
-                        </Row>
-                    ) :
-                        <>
-                            <Collection />
-                            <Collection />
-                            <Collection />
-                            <Collection />
-                            <Collection />
-                        </>
-                    }
-                </Col>
-                <Col sm={4} className='d-none d-sm-block'>
-                    <Tags />
-                </Col>
+              <Col className='col-6'>
+                <Item />
+              </Col>
+              <Col className='col-6'>
+                <Item />
+              </Col>
+              <Col className='col-6'>
+                <Item />
+              </Col>
+              <Col className='col-6'>
+                <Item />
+              </Col>
             </Row>
-            <div className='d-flex justify-content-center m-3'>
-                <Button variant="primary" className='mx-4'>See more</Button>
-            </div>
-        </Container>
-    )
+          ) :
+            <>
+              <Collection />
+              <Collection />
+              <Collection />
+              <Collection />
+              <Collection />
+            </>
+          }
+        </Col>
+        <Col sm={4} className='d-none d-sm-block'>
+          <Tags />
+        </Col>
+      </Row>
+      <div className='d-flex justify-content-center m-3'>
+        <Button variant="primary" className='mx-4'>See more</Button>
+      </div>
+    </Container>
+  )
 }
 
 export default Home
