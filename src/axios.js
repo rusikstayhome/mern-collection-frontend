7d8f87cebc2531f510998fc6b2a1774cb7f95891
rusikstayhome <alenrus32@gmail.com>
2022-10-27 22:35:50 +0300

    added process.env

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3001'
+    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3001'
 })
 
 instance.interceptors.request.use((config) => {
